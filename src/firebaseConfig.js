1165e34425b9d6b186aff5a345913e490cca0c1d
Jabz-Jab <gitprojectjabz@gmail.com>
2026-03-23 21:17:25 +0800

    remove API key

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,15 +2,6 @@ import { initializeApp }    from "firebase/app";
 import { getFirestore }     from "firebase/firestore";
 import { getAuth }          from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDCs_DUw3b7F3aYgpw26RZKbeY2mOLI9Dc",
-  authDomain: "project-montero.firebaseapp.com",
-  projectId: "project-montero",
-  storageBucket: "project-montero.firebasestorage.app",
-  messagingSenderId: "358910689799",
-  appId: "1:358910689799:web:d53bba6e0d2315e991a0a2"
-};
-
 const app  = initializeApp(firebaseConfig);
 export const db   = getFirestore(app);
 export const auth = getAuth(app);
